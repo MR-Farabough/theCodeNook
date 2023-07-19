77776845dba6222fdd564d222c9e7636327bd2c4
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-18 22:50:13 -0500

    Add Nav Component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import App from './App';
+import Nav from './components/Nav';
 import errorPage from './components/errorPage';
 
 const Router = () => {
 	return (
-		<Routes>
-			<Route path="/" Component={App} />
-			<Route path="*" Component={errorPage} />
-		</Routes>
+		<>
+			<Nav />
+			<Routes>
+				<Route path="/" Component={App} />
+				<Route path="*" Component={errorPage} />
+			</Routes>
+		</>
 	);
 };
 
