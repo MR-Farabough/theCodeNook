78148f7539d99d04821b0b46ea915c7cd8c60a5f
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-19 23:35:04 -0500

    Basic dropdown

---
+++ src/components/ProfileDropDown.tsx
@@ -0,0 +1,13 @@
+const profileDropdown = () => {
+	return (
+		<div className="dropdown">
+			<div className="dropdown-content">
+				<a href="#">Link 1</a>
+				<a href="#">Link 2</a>
+				<a href="#">Link 3</a>
+			</div>
+		</div>
+	);
+};
+
+export default profileDropdown;
