7faeb4b7f97a3d03b86f9e4867050af9fdc06e4b
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-29 19:58:22 -0500

    SubNav Content on hover and basic styling

---
+++ src/components/SubNav.tsx
@@ -0,0 +1,133 @@
+import { useState } from 'react';
+import '../styles/SubNav.css';
+
+const SubNav = () => {
+	const [dsaMenu, setDsaMenu] = useState<Boolean>(false);
+	const [webDevMenu, setWebDevMenu] = useState<Boolean>(false);
+	const [languageMenu, setLangugageMenu] = useState<Boolean>(false);
+	const [dbMenu, setDbMenu] = useState<Boolean>(false);
+	const [devPracticeMenu, setDevPracticeMenu] = useState<Boolean>(false);
+
+	const handleMenuToggle = (menu: string) => {
+		switch (menu) {
+			case 'dsa':
+				dsaMenu ? setDsaMenu(false) : setDsaMenu(true);
+				break;
+			case 'webDev':
+				webDevMenu ? setWebDevMenu(false) : setWebDevMenu(true);
+				break;
+			case 'languages':
+				languageMenu ? setLangugageMenu(false) : setLangugageMenu(true);
+				break;
+			case 'database':
+				dbMenu ? setDbMenu(false) : setDbMenu(true);
+				break;
+			case 'devPractices':
+				devPracticeMenu ? setDevPracticeMenu(false) : setDevPracticeMenu(true);
+				break;
+		}
+	};
+
+	return (
+		<nav className="sub-nav">
+			<div
+				onMouseEnter={() => handleMenuToggle('dsa')}
+				onMouseLeave={() => handleMenuToggle('dsa')}
+				className="sub-menu"
+			>
+				<p className="subMenu-topic">Data Structures & Algorithms</p>
+				{dsaMenu && (
+					<div className="subNav-dropdown">
+						<ul>
+							<li>Arrays & Strings</li>
+							<li>Linked Lists</li>
+							<li>Stacks & Queues</li>
+							<li>Trees & Graphs</li>
+							<li>Sorting & Searching Algorithms</li>
+							<li>Dynamic Programming</li>
+							<li>Big O Notation & Complexity Analysis</li>
+						</ul>
+					</div>
+				)}
+			</div>
+
+			<div
+				onMouseEnter={() => handleMenuToggle('webDev')}
+				onMouseLeave={() => handleMenuToggle('webDev')}
+				className="sub-menu"
+			>
+				<p className="subMenu-topic">Web Development</p>
+				{webDevMenu && (
+					<div className="subNav-dropdown">
+						<ul>
+							<li>HTML & CSS</li>
+							<li>Front-end Frameworks</li>
+							<li>Back-end Frameworks</li>
+							<li>Web APIs & Restful Services</li>
+							<li>Web Security & Best Practices</li>
+							<li>Responsive Design & Cross-Browser Compatibility</li>
+						</ul>
+					</div>
+				)}
+			</div>
+
+			<div
+				onMouseLeave={() => handleMenuToggle('languages')}
+				onMouseEnter={() => handleMenuToggle('languages')}
+				className="sub-menu"
+			>
+				<p className="subMenu-topic">Languages</p>
+				{languageMenu && (
+					<div className="subNav-dropdown">
+						<ul>
+							<li>Python</li>
+							<li>Java</li>
+							<li>JavaScript / TypeScript</li>
+							<li>C / C++</li>
+							<li>Ruby</li>
+							<li>Swift</li>
+						</ul>
+					</div>
+				)}
+			</div>
+
+			<div
+				onMouseLeave={() => handleMenuToggle('database')}
+				onMouseEnter={() => handleMenuToggle('database')}
+				className="sub-menu"
+			>
+				<p className="subMenu-topic">Database Management</p>
+				{dbMenu && (
+					<div className="subNav-dropdown">
+						<p>Database Management</p>
+						<br />
+						<p>More To Come!</p>
+					</div>
+				)}
+			</div>
+
+			<div
+				onMouseLeave={() => handleMenuToggle('devPractices')}
+				onMouseEnter={() => handleMenuToggle('devPractices')}
+				className="sub-menu"
+			>
+				<p className="subMenu-topic">Development Practices</p>
+				{devPracticeMenu && (
+					<div className="subNav-dropdown">
+						<ul>
+							<li>Agile Development & Scrum</li>
+							<li>Test-Drive Development (TDD)</li>
+							<li>Continuous Integration & Deployment</li>
+							<li>Code Review & Best Practices</li>
+							<li>Design Patterns & Principles</li>
+							<li>Version Control (GIT)</li>
+							<li>Performance Optimization Techniques</li>
+						</ul>
+					</div>
+				)}
+			</div>
+		</nav>
+	);
+};
+
+export default SubNav;
