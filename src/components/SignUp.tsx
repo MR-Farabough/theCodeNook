0d90d4df89a4a22275788b0e2abd9ec1dfe736b4
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-08-08 12:11:46 -0500

    Only allows for o'Auth

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,47 +1,52 @@
-import { useNavigate } from 'react-router-dom';
 import '../styles/SignUp.css';
 import githubLogo from '../assets/github.png';
 import discordLogo from '../assets/discord.png';
-import googleLogo from '../assets/google.png';
+import supabase from '../lib/supabaseClient';
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../Router';
+import { getUserStatus } from '../functions/getUserStatus';
 
 const SignIn = () => {
-	const navigate = useNavigate();
+	const { setUser } = useContext(UserContext);
+	useEffect(() => {
+		const fetchUserStatus = async () => {
+			try {
+				const userStatus = await getUserStatus();
+				setUser(userStatus);
+			} catch (error) {
+				console.error('Error fetching user status:', error);
+				setUser(null);
+			}
+		};
+		fetchUserStatus();
+	}, []);
+
+	const gitHubLogin = async () => {
+		await supabase.auth.signInWithOAuth({
+			provider: 'github',
+		});
+	};
+
+	const discordLogin = async () => {
+		await supabase.auth.signInWithOAuth({
+			provider: 'discord',
+		});
+	};
+
 	return (
 		<section className="signUp-form">
-			<div className="input-option">
-				<label htmlFor="first-name">First Name</label>
-				<input id="first-name" type="text" placeholder="Enter First Name..." />
-			</div>
-			<div className="input-option">
-				<label htmlFor="last-name">Last Name</label>
-				<input id="last-name" type="text" placeholder="Enter Last Name..." />
-			</div>
-			<div className="input-option">
-				<label htmlFor="username">Username</label>
-				<input id="username" type="text" placeholder="Enter Username..." />
-			</div>
-			<div className="input-option">
-				<label htmlFor="email">Email</label>
-				<input id="email" type="text" placeholder="Enter Email..." />
-			</div>
-			{/* TODO Style sign up add already a user */}
-			{/* TODO 3rd party auths */}
-			<button className="signUp-button">Sign Up</button>
-			<div className="signUp-seperator">
-				<hr />
-				<div className="or">or</div>
-				<hr />
-			</div>
-			<div className="third-party-signup">
-				<img className="thirdPartyLogo" src={githubLogo} alt="github-logo" />
-				<img className="thirdPartyLogo" src={discordLogo} alt="discord-logo" />
-				<img className="thirdPartyLogo" src={googleLogo} alt="google-logo" />
-			</div>
-			<div className="lower-signup">
-				<p>Already Have an Account?</p>
-				<a className="signIn-tag" onClick={() => navigate('/signin')}>
-					Sign In
-				</a>
+			<div className="flex">
+				<div onClick={gitHubLogin} className="third-party-signup">
+					<img className="thirdPartyLogo" src={githubLogo} alt="github-logo" />
+				</div>
+				<div onClick={discordLogin} className="third-party-signup">
+					<img
+						className="thirdPartyLogo"
+						src={discordLogo}
+						alt="discord-logo"
+					/>
+				</div>
 			</div>
 		</section>
 	);
