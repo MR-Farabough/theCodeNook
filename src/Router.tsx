1e4d9c573673e4cba1ff270aaa5945b42c1dafe8
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-08-08 12:12:14 -0500

    Global Context for user to db

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,21 +6,33 @@ import Donation from './components/Donation';
 import Newsletter from './components/Newsletter';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
+import { createContext, useState } from 'react';
+import Profile from './components/Profile';
 
+export const UserContext = createContext<any>(null);
+export const LoadingContext = createContext<any>(false);
 const Router = () => {
+	const [user, setUser] = useState<null | Boolean>(null);
+	const [loadStatus, setLoadStatus] = useState<null | Boolean>(null);
+	// Figure out what is using the above state / context
 	return (
 		<>
-			<Header />
-			<main>
-				<Routes>
-					<Route path="/" Component={App} />
-					<Route path="/donation" Component={Donation} />
-					<Route path="/newsletter" Component={Newsletter} />
-					<Route path="/signup" Component={SignUp} />
-					<Route path="/signin" Component={SignIn} />
-					<Route path="*" Component={errorPage} />
-				</Routes>
-			</main>
+			<UserContext.Provider
+				value={{ user, setUser, loadStatus, setLoadStatus }}
+			>
+				<Header />
+				<main>
+					<Routes>
+						<Route path="/" Component={App} />
+						<Route path="/donation" Component={Donation} />
+						<Route path="/newsletter" Component={Newsletter} />
+						<Route path="/signup" Component={SignUp} />
+						<Route path="/signin" Component={SignIn} />
+						<Route path="/profile" Component={Profile} />
+						<Route path="*" Component={errorPage} />
+					</Routes>
+				</main>
+			</UserContext.Provider>
 		</>
 	);
 };
